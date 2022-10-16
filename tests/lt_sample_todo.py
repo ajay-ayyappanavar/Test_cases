86aa49eeeaa2a989f0cb49d041240e4e5bb530ce
ajay-ayyappanavar <115933780+ajay-ayyappanavar@users.noreply.github.com>
2022-10-16 22:28:17 +0530

    Update lt_sample_todo.py

--- tests/lt_sample_todo.py
+++ tests/lt_sample_todo.py
@@ -30,6 +30,7 @@ class TestLink:
         :return: None
         """
         driver.get(r'https://www.lambdatest.com/selenium-playground/')
+        driver.implicitly_wait(10)
         driver.find_elements(By.XPATH, "//a[contains(text(),'Drag & Drop Sliders')]").click()
         
         elem = driver.find_element(By.XPATH,"//body/div[@id='__next']/div[1]/section[3]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/input[1]")
@@ -37,4 +38,32 @@ class TestLink:
         ac.move_to_element(elem).move_by_offset(946, 349).click().perform()
         val = driver.find_element(By.ID,"rangeSuccess").text
         assert val == '95' , " Value is not equal to 95"
-       
+    
+    def test_validation(self, driver):
+        
+        driver.get(r'https://www.lambdatest.com/selenium-playground')
+        driver.implicitly_wait(10)
+        driver.find_element(By.XPATH, '//a[contains(text(),'Input Form Submit')]').click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//button[contains(text(),'Submit')]').click()
+        name = driver.find_element(By.XPATH, '//input[@id='name']')
+        verify = "Please fill in the fileds"
+        msg = name.getattr(name,"validationMessage")
+        assert verify == msg, " validation message is not as ecpected"
+        name.send_keys('Shiva')
+        driver.find_element(By.CSS_SELECTOR,'#inputEmail4').send_keys('shiva@email.com')
+        driver.find_element(By.CSS_SELECTOR,'#inputPassword4').send_keys('shiva@1234')
+        driver.find_element(By.XPATH,'//input[@id='company']').send_keys('xyz')
+        driver.find_element(By.XPATH,'//input[@id='websitename']').send_keys('abs')
+        select = driver.find_element(By.XPATH, '//body/div[@id='__next']/div[1]/section[3]/div[1]/div[1]/div[2]/div[1]/form[1]/div[3]/div[1]/select[1]').click()
+        select.select_by_visible_text('United States')
+        driver.find_element(By.CSS_SELECTOR,'#inputCity').send_keys('chik')
+        driver.find_element(By.CSS_SELECTOR,'#inputAddress1').send_keys('anything')
+        driver.find_element(By.CSS_SELECTOR,'#inputAddress2').send_keys('allgod')
+        driver.find_element(By.XPATH,'//input[@id='inputState']').send_keys('KA')
+        driver.find_element(By.CSS_SELECTOR,'#inputZip').send_keys('675435')
+        driver.find_element(By.XPATH, '//button[contains(text(),'Submit')]').click()
+        
+        final_msg = 'Thanks for contacting us, we will get back to you '
+        equal = driver.find_element(By.XPATH,"//p[contains(text(),'Thanks for contacting us, we will get back to you ')]").text
+        assert final_msg == equal, "Message is not as expected"
